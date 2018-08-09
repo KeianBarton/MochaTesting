7f1f490a1493c46a48b7bcc124e78dea7fad1a15
KeianBarton <Keian.Barton@civica.co.uk>
2018-08-09 15:26:24 +0100

    Getting Mocha working with timeouts

---
+++ test/controllers/auth.controller.spec.js
@@ -0,0 +1,29 @@
+var assert = require('assert');
+var authController = require('../../controllers/auth.controller');
+
+describe('AuthController', function(){
+    describe('isAuthorized', function() {
+        it('Should return false if not authorized', function() {
+            assert.equal(false, authController.isAuthorized(['user'], 'admin'));
+        });
+        it('Should return true if authorized', function() {
+            assert.equal(true, authController.isAuthorized(['user', 'admin'], 'admin'));
+        });
+    });
+    describe('isAuthorizedAsync', function() {
+        it('Should return false if not authorized', function(done) {
+            this.timeout(2500);
+            authController.isAuthorizedAsync(['user'], 'admin', function(isAuth){
+                assert.equal(false, isAuth);
+                done();
+            });
+        });
+        it('Should return true if authorized', function(done) {
+            this.timeout(2500);
+            authController.isAuthorizedAsync(['user', 'admin'], 'admin', function(isAuth){
+                assert.equal(true, isAuth);
+                done();
+            });
+        });
+    });
+});
---
+++ test/starting.spec.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+
+describe('Basic Mocha Test', function() {
+    it('should throw errors', function() {
+        // assert.equal(2, 3);
+    });
+    it('assertion error caught', function() {
+        // try {
+        //     assert.equal(2, 3);
+        // } catch (e) {
+        //     console.log(e);
+        // }
+    });
+});
