2e150df21d2a15567652bfd9705050f343616382
KeianBarton <Keian.Barton@civica.co.uk>
2018-08-13 14:59:47 +0100

    Adding hooks

--- test/controllers/auth.controller.spec.js
+++ test/controllers/auth.controller.spec.js
@@ -2,25 +2,31 @@ var assert = require('assert');
 var authController = require('../../controllers/auth.controller');
 
 describe('AuthController', function(){
+    beforeEach(function() {
+        console.log('Running before each');
+        authController.setRoles(['user']);
+    });
     describe('isAuthorized', function() {
         it('Should return false if not authorized', function() {
-            assert.equal(false, authController.isAuthorized(['user'], 'admin'));
+            assert.equal(false, authController.isAuthorized('admin'));
         });
         it('Should return true if authorized', function() {
-            assert.equal(true, authController.isAuthorized(['user', 'admin'], 'admin'));
+            authController.setRoles(['user', 'admin']);
+            assert.equal(true, authController.isAuthorized('admin'));
         });
     });
     describe('isAuthorizedAsync', function() {
         it('Should return false if not authorized', function(done) {
             this.timeout(2500);
-            authController.isAuthorizedAsync(['user'], 'admin', function(isAuth){
+            authController.isAuthorizedAsync('admin', function(isAuth){
                 assert.equal(false, isAuth);
                 done();
             });
         });
         it('Should return true if authorized', function(done) {
             this.timeout(2500);
-            authController.isAuthorizedAsync(['user', 'admin'], 'admin', function(isAuth){
+            authController.setRoles(['user', 'admin']);
+            authController.isAuthorizedAsync('admin', function(isAuth){
                 assert.equal(true, isAuth);
                 done();
             });
